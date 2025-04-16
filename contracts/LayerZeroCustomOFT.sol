a1c4e4c7dcdbae6fe657da346f4b89f589204978
Steven Enamakel <enamakel@maha.xyz>
2025-04-16 14:10:34 +0530

    deployed to berachain

--- contracts/LayerZeroCustomOFT.sol
+++ contracts/LayerZeroCustomOFT.sol
@@ -19,38 +19,34 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract LayerZeroCustomOFT is OFT, ERC20Permit {
-  mapping(address => bool) public blacklist;
-  bool public paused;
+    mapping(address => bool) public blacklist;
+    bool public paused;
 
-  constructor(
-    string memory name,
-    string memory symbol,
-    address _lzEndpoint
-  )
-    OFT(name, symbol, _lzEndpoint, msg.sender)
-    Ownable(msg.sender)
-    ERC20Permit(symbol)
-  {
-    _mint(msg.sender, 1e18);
-    _burn(msg.sender, 1e18);
-  }
+    constructor(string memory name, string memory symbol, address _lzEndpoint)
+        OFT(name, symbol, _lzEndpoint, msg.sender)
+        Ownable(msg.sender)
+        ERC20Permit(symbol)
+    {
+        _mint(msg.sender, 1e18);
+        _burn(msg.sender, 1e18);
+    }
 
-  function toggleBlacklist(address who, bool what) external onlyOwner {
-    blacklist[who] = what;
-  }
+    function toggleBlacklist(address who, bool what) external onlyOwner {
+        blacklist[who] = what;
+    }
 
-  function togglePause(bool what) external onlyOwner {
-    paused = what;
-  }
+    function adminMint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
 
-  function _update(
-    address from,
-    address to,
-    uint256 value
-  ) internal virtual override {
-    require(!blacklist[from], "blacklisted");
-    require(!blacklist[to], "blacklisted");
-    require(!paused, "paused");
-    super._update(from, to, value);
-  }
+    function togglePause(bool what) external onlyOwner {
+        paused = what;
+    }
+
+    function _update(address from, address to, uint256 value) internal virtual override {
+        require(!blacklist[from], "blacklisted");
+        require(!blacklist[to], "blacklisted");
+        require(!paused, "paused");
+        super._update(from, to, value);
+    }
 }
